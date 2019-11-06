1995ede6b777f76ae828694ff1b3485470dc4b8e
Matthew Brennecke <mpbrenn@umich.edu>
2019-11-06 18:42:58 -0500

    Homework 2.1
    Added test to suite for exponentiation operator ^.

--- test_rpn.py
+++ test_rpn.py
@@ -17,6 +17,9 @@ class TestBasics(unittest.TestCase):
     def test_div2(self):
         result = rpn.calculate('24 8 /')
         self.assertEqual(3, result)
+    def test_exp(self):
+        result = rpn.calculate('4 3 ^')
+        self.assertEqual(64, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
