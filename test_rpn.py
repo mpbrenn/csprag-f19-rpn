1330d23772dc51172c742299b6499e0d91c9d885
Matthew Brennecke <mpbrenn@umich.edu>
2019-11-06 18:24:00 -0500

    Initial commit
    Adding rpn calculator implementation from lecture

---
+++ test_rpn.py
@@ -0,0 +1,23 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
+    def test_sub(self):
+        result = rpn.calculate('5 3 -')
+        self.assertEqual(2, result)
+    def test_mul(self):
+        result = rpn.calculate('12 8 *')
+        self.assertEqual(96, result)
+    def test_div1(self):
+        result = rpn.calculate('12 8 /')
+        self.assertEqual(1, result)
+    def test_div2(self):
+        result = rpn.calculate('24 8 /')
+        self.assertEqual(3, result)
+    def test_badinput(self):
+        with self.assertRaises(TypeError):
+            rpn.calculate('1 2 3 +')
+        
